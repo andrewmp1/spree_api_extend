711cb8e1548f0deb7d7a5de971a8c5a59d0b1e23
Drew Purdy <andrewmp1@gmail.com>
2013-06-04 15:08:25 -0700

    Add token to order attributes returned in api

--- app/helpers/spree/api_helpers_decorator.rb
+++ app/helpers/spree/api_helpers_decorator.rb
@@ -2,4 +2,8 @@ Spree::Api::ApiHelpers.class_eval do
   def user_attributes
     [:id, :email, :created_at, :updated_at, :spree_api_key]
   end
+
+  def order_attributes
+    [:id, :number, :item_total, :total, :state, :adjustment_total, :user_id, :created_at, :updated_at, :completed_at, :payment_total, :shipment_state, :payment_state, :email, :special_instructions, :token]
+  end
 end
